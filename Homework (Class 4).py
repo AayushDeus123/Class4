9b01b9b1b08f693028d8b92979b509085a95d128
AayushDeus123 <aayushdeus312@gmail.com>
2024-12-09 20:39:29 +0530

    Add files via upload

---
+++ Homework (Class 4).py
@@ -0,0 +1,6 @@
+#Calculating square root
+print('Finding the square root of any given number')
+import math
+user = int(input('\nEnter your number : '))
+sqroot = (math.sqrt(user))
+print('The Square root is' , sqroot)
